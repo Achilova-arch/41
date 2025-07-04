5ee9c7e11ea5f6afddac8ce8d021c8727102a01b
Achilova-arch <asalachilova96@gmail.com>
2025-07-04 10:09:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let randomSon=Math.floor(Math.random()*90+10);
+alert("Sizda hozir random chiqishni boshlaydi:😉")
+alert( "Random orqali chiqan son:"+ randomSon)
+confirm("Biz bu sonni 2 ko'paytimoqchimiz!!")
+  alert(`Random orqali chiqan son${randomSon}\n${randomSon} 2ga ko'paytirdik:${random*2}\n${randomSon} 2ga boldik:${random : 2 }\n${randomSon} 3ga qoldqli boldik: ${randomSon:3 }`)
